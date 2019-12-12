67a1435508f53c31962b6bc0479a2525d47c480c
Jennifer Wong <jenn.wong@modeanalytics.com>
2019-12-12 15:29:46 -0800

    add moar stuff

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 // List of garbage images and the correct bin as objects
 let garbage = [
   {
+    name: 'Green Bottle',
     path: 'img/garbage/green_bottle.png',
     bin: 'recycle',
     err: false
   },
   {
+    name: 'Battery',
     path: 'img/garbage/battery.png',
     bin: 'special',
     err: false
   },
   {
+    name: 'Batt 1',
     path: 'img/garbage/battery.png',
     bin: 'special',
     err: false
   },
   {
+    name: 'Batt 2',
     path: 'img/garbage/battery.png',
     bin: 'special',
     err: false
@@ -27,22 +31,24 @@ let garbage = [
 let numItems = 2;
 let counter = 0;
 let wrongAnswers = 0;
-let timer = 0;
+let time = 0;
 
 // Start the game
 // Show the first garbage image, 
 let handleStart = () => {
-  console.log('start the game and the timer');
+  $('.timer-min').html(time);
+  time = setInterval(timer, 1000);
   $('.header').fadeOut(300);
   $('.main-game').fadeIn(1500);
   showNextImg();
 }
 
 let endGame = () => {
+  clearInterval(time);
   $('.main-game').fadeOut(300);
   $('.score').fadeIn(1500);
 
-  let finalScore = timer + wrongAnswers;
+  let finalScore = time + wrongAnswers;
   $('.final-score').html(finalScore);
 }
 
@@ -53,7 +59,7 @@ let showNextImg = () => {
     endGame();
     return;
   }
-
+  $('.garbage-title').html(garbage[counter].name)
   $('.garbage-img').attr('src', garbage[counter].path);
 }
 
@@ -67,4 +73,10 @@ let handleBinClick = (e) => {
   }
   counter++;
   showNextImg();
+}
+
+let timer = () => {
+  console.log(time);
+  time++;
+  $('.timer-min').html(time);
 }
